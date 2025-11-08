1d886b3f0a7f8524d23844d7b666cc401ba884c6
Ugo Chinemerem <ugofranklin22@gmail.com>
2025-11-08 10:48:40 -0500

    fix

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
   email: "ugofranklin22@gmail.com",
   phone: "",
-  address: "37 Whiteface Crescent, Brampton, Canada",
+  address: "",
   github: "https://github.com/MeremArt",
   linkedIn: "https://www.linkedin.com/in/chinemerem-ugo-23910916b/",
   twitter: "https://x.com/merem___",
